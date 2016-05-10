e5b14c3c69e01b55d928f953750557b846a1ab77
Ondřej Marek <on.dude.ma.65@gmail.com>
2016-05-10 02:56:52 +0200

    ProxyDriver - Driver Property info
    ProxyDriver.java
    implemented getPropertyInfo(String, Properties)

--- src/org/fit/jdbc_proxy_driver/implementation/ProxyDriver.java
+++ src/org/fit/jdbc_proxy_driver/implementation/ProxyDriver.java
@@ -69,11 +69,51 @@ public class ProxyDriver implements Driver{
 	}
 
 	@Override
-	public DriverPropertyInfo[] getPropertyInfo(String arg0, Properties arg1)
-			throws SQLException {
-		throw new UnsupportedOperationException("Not supported yet");
+	public DriverPropertyInfo[] getPropertyInfo(String url, Properties info) throws SQLException {
+		int items = 2;
+		
+		if (info != null && info.containsKey("items")) {
+			try {
+				items = Integer.parseInt(info.getProperty("items"));
+				
+				if (items <= 0) {
+					items = 2;
+				}
+			} catch (NumberFormatException e) {
+			}
+		}
+		
+		DriverPropertyInfo[] res = new DriverPropertyInfo[2 + 6 * items];
+		
+		res[0] = new DriverPropertyInfo("items", "Number of connnections to databases.");
+		res[1] = new DriverPropertyInfo("default", "Specifies primary connection by name.");
+		
+		res[0].required = true;
+		res[1].required = false;
+		
+		for (int i = 0; i < items; i++) {
+			res[i * 6 + 2] = new DriverPropertyInfo("db" + i + "_driver", "Specifies the class for database driver.");
+			res[i * 6 + 2].required = true;
+			
+			res[i * 6 + 3] = new DriverPropertyInfo("db" + i + "_url", "Specifies the database url. It may contains additional information such as user name or password.");
+			res[i * 6 + 3].required = true;
+			
+			res[i * 6 + 4] = new DriverPropertyInfo("db" + i + "_name", "Unique name to mark database connection.");
+			res[i * 6 + 4].required = true;
+			
+			res[i * 6 + 5] = new DriverPropertyInfo("db" + i + "_user", "Specifies the database user name.");
+			res[i * 6 + 5].required = false;
+			
+			res[i * 6 + 6] = new DriverPropertyInfo("db" + i + "_password", "Specifies the password to database.");
+			res[i * 6 + 6].required = false;
+			
+			res[i * 6 + 7] = new DriverPropertyInfo("db" + i + "_regexp", "Specifies regular expression that is going to be associated to this connection.");
+			res[i * 6 + 7].required = true;
+		}
+		
+		
+		return res;
 	}
-
 	@Override
 	public boolean jdbcCompliant() {
 		return false;
