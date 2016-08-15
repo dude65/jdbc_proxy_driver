e05f84b6a9f171229e89e65ffb55c3a34a4124ef
Ondřej Marek <on.dude.ma.65@gmail.com>
2016-08-16 00:08:50 +0200

    Configuration
    added configuration class - ProxyConfiguration.java. Contains information about date format now.

---
+++ src/main/java/org/fit/proxy/jdbc/configuration/ProxyConfiguration.java
@@ -0,0 +1,40 @@
+package org.fit.proxy.jdbc.configuration;
+
+import java.util.Date;
+
+import org.apache.commons.lang3.time.DateFormatUtils;
+
+/**
+ * This class contains configuration information of proxy driver.
+ * 
+ * @author Ondřej Marek
+ *
+ */
+public class ProxyConfiguration {
+	private static String dateFormat = "%H:%M:%S %d.%m.%Y";
+
+	public static String getDateFormat() {
+		return dateFormat;
+	}
+
+	public static void setDateFormat(String dateFormat) {
+		ProxyConfiguration.dateFormat = dateFormat;
+	}
+	
+	/**
+	 * Returns a formated current date
+	 * @return formated date
+	 */
+	public static String getCurrentParsedDate() {
+		return DateFormatUtils.format(new Date(System.currentTimeMillis()), dateFormat);
+	}
+	
+	/**
+	 * Returns a formated date
+	 * @param date date to be formated
+	 * @return formated date
+	 */
+	public static String getParsedDate(Date date) {
+		return DateFormatUtils.format(date, dateFormat);
+	}
+}
